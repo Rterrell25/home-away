109c96f7e6ac6f3ef2357e541d5e9d81802688d8
Rob Terrell <terrellr700@gmail.com>
2024-07-18 15:24:29 -0400

    added base structure for  drop down for nav component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,6 @@
-const UserIcon = () => {
-  return <div>User Icon</div>
-}
+import { LuUser2 } from 'react-icons/lu'
 
+const UserIcon = () => {
+  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />
+}
 export default UserIcon
