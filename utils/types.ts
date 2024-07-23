04c6cb3e7dea44319c46f7224d0c33e3b04090f9
Rob Terrell <terrellr700@gmail.com>
2024-07-23 11:36:25 -0400

    updated providers for calendly integration

--- utils/types.ts
+++ utils/types.ts
@@ -2,9 +2,3 @@ export type actionFunction = (
   prevState: any,
   formData: FormData
 ) => Promise<{ message: string }>
-
-export interface ImageData {
-  id: Record<string, string>
-  tableName: string
-  fieldName: string
-}
